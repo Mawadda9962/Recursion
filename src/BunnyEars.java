b8c313d72a920f5aea83e0f2ba15ffcd69d920b9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:42:00 +0400

    Creating a method by name bunnyEars2

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -1,5 +1,13 @@
 public class BunnyEars {
     public static void main(String[] args){
 
+
+
+    }
+
+    public int bunnyEars2(int n){
+        if(n == 1){
+            return 1;
+        }
     }
 }

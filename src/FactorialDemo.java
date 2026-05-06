a6a3447c5b2b9fd1f887d277665f2ce19ad05d77
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:34:57 +0400

    Calling factorial() method using the object

--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -1,7 +1,7 @@
 public class FactorialDemo {
     public static void main(String[] args){ //Creating another Class
         Factorial factorial = new Factorial();//Creating an object
-        System.out.println(factorial.factorial(2));
+        System.out.println(factorial.factorial(2));//Calling the function by using method
 
     }
 }

76afccaf4f466023bb107fe0afb8018fd58cc6a7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:48:10 +0400

    Creating the object

--- src/BunnyEarsDemo.java
+++ src/BunnyEarsDemo.java
@@ -1,7 +1,7 @@
 public class BunnyEarsDemo {
     public static void main(String[] args){ //Creating the main method
 
-        BunnyEars bunnyEars = new BunnyEars();
+        BunnyEars bunnyEars = new BunnyEars(); //Creating the object
 
     }
 }

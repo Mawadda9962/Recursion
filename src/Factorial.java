b66b11a77ce29f904066fa44c8363fb08a019474
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:15:24 +0400

    Adding base case

--- src/Factorial.java
+++ src/Factorial.java
@@ -5,7 +5,7 @@ public class Factorial {
 
     public static int factorial(int n){
         if(n == 1){
-            return 1; //
+            return 1; //base case
         }else {
             return factorial(n-1) * n;
         }

0797116d477d54b035e5f96857f619eea7716782
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:50:31 +0400

    Calling the function

--- src/BunnyEarsDemo.java
+++ src/BunnyEarsDemo.java
@@ -2,6 +2,7 @@ public class BunnyEarsDemo {
     public static void main(String[] args){ //Creating the main method
 
         BunnyEars bunnyEars = new BunnyEars(); //Creating the object
+        System.out.println(bunnyEars.bunnyEars2());
 
     }
 }

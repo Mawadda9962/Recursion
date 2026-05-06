a405720d1d077063fad7797b35ecb2d9dffb45d9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:32:15 +0400

    Creating another Class

---
+++ src/FactorialDemo.java
@@ -0,0 +1,7 @@
+public class FactorialDemo {
+    public static void main(String[] args){
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.factorial(5));
+
+    }
+}

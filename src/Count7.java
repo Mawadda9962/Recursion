be40bda78592b91098bd543e785b89e22a72e1cd
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 16:06:54 +0400

    Check if the last digit is equal to 7

--- src/Count7.java
+++ src/Count7.java
@@ -6,7 +6,9 @@ public class Count7 {
         int lastDigit = n % 10; // Getting the last digit using
         int removeDigit = n / 10; //Removing the last digit
 
-        if(lastDigit == )
+        if(lastDigit == 7){ //Check if the last digit is equal to 7
+            return 1 + count7(n/10);
+        }else
 
     }
 }

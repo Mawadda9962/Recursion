973cbcd48ba21f796b4f7e6462a8b3f902792b04
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:12:39 +0400

    Defining the base case

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,3 +1,13 @@
 public class Factorial {
+    public static void main(String[] args){
 
+    }
+
+    public static int factorial(int n){
+        if(n == 1){
+            return 1; //
+        }else {
+            return factorial(n-1) * n;
+        }
+    }
 }

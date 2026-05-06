7e167967effbe29e3c3cd0b2c1c933c4c9e243f5
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 16:46:47 +0400

    Checking if the string starts with

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -3,6 +3,14 @@ public class ChangePi {
         if (str.length() == 0){ //base Case
             return "";
         }
+
+
+        if (str.startsWith("pi")){ //Checking if the string starts with "pi"
+            return "3.14" +changePi(str.substring(2));
+        }else{
+            str.charAt(0);
+        }
+
     }
 
 }

cdab98b332888df68b3b635e87126972d4af8ca2
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 16:56:25 +0400

    Testing the method

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -1,7 +1,7 @@
 public class ChangePiDemo {
     public static void main(String[] args){
-        ChangePiDemo changePiDemo = new ChangePiDemo(); //Creating an object
-        System.out.println();
+        ChangePi changePi = new ChangePi(); //Creating an object
+        System.out.println(changePi.changePi("xpix")); //Testing the method
 
 
     }

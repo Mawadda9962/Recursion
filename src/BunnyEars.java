6873dc31cce854d4da3d4f522c1ec753f78db4fe
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 15:45:24 +0400

    Cheacking the bunny nnumber is odd

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -9,8 +9,10 @@ public class BunnyEars {
         if(n == 1){
             return 1;// Base Case
         }else if (n % 2 == 0){
-            return 3 + bunnyEars2(n-1);
+            return 3 + bunnyEars2(n-1); // Checking the bunny number is even
         }else{
+            return 2 + bunnyEars2(n-1);
+
 
         }
     }

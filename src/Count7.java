25c8f1bebe0e29ed52c2f269b2589a399e7bb95e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 16:05:36 +0400

    Removing the last digit

--- src/Count7.java
+++ src/Count7.java
@@ -4,7 +4,7 @@ public class Count7 {
             return 0;//Base Case
         }
         int lastDigit = n % 10; // Getting the last digit using
-        int removeDigit = n / 10;
+        int removeDigit = n / 10; //Removing the last digit
 
         if(lastDigit == )
 

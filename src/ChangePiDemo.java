ec1e4267e81fcc2cf3f3d69c1b1e5235675eb608
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-06 16:53:39 +0400

    Creating an object

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -1,5 +1,7 @@
 public class ChangePiDemo {
     public static void main(String[] args){
+        ChangePiDemo changePiDemo = new ChangePiDemo(); //Creating an object
+        System.out.println();
 
 
     }

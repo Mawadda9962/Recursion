f26e062c540b5e745832d3fc5f45751eca801695
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 08:40:14 +0400

    fixing the part of Calling the array

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -2,7 +2,8 @@ public class Array11Demo {
     public static void main(String[] args){ // Creating the main method
         Array11 array11 = new Array11(); // Creating an object
 
-        System.out.println(array11.array11([1, 2, 11], 0)); //Calling the array
+        int[] nums = {1, 2, 11};
+        System.out.println(array11.array11(nums, 0)); //Calling the array
 
 
     }

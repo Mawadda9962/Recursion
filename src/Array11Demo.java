ef09a5d354430ee2e98a64299558fa01ff56fe79
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 08:37:07 +0400

    Calling the array

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -2,6 +2,8 @@ public class Array11Demo {
     public static void main(String[] args){ // Creating the main method
         Array11 array11 = new Array11(); // Creating an object
 
+        System.out.println(array11.array11([1, 2, 11], 0)); //Calling the array
+
 
     }
 }

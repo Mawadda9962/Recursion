dad5b03d4cba7f979c1a29112353e77b72a420ef
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 10:02:42 +0400

    Defining the base class

--- src/BunnyEars2.java
+++ src/BunnyEars2.java
@@ -1,8 +1,10 @@
 public class BunnyEars2 {
     public int bunnyEars(int n){
-        if (n == 0){
-            return
+        if (n == 0){ //Base Class
+            return 0;
         }
+
+
     }
 
 }

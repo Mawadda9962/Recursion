2ecae1d48ce82843eb9da9f7e4fb21053c5c2686
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 08:33:39 +0400

    Create an object

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -1,6 +1,6 @@
 public class Array11Demo {
-    public static void main(String[] args){ // Create the main method
-        Array11 array11 = new Array11();
+    public static void main(String[] args){ // Creating the main method
+        Array11 array11 = new Array11(); // Creating an object
 
 
     }

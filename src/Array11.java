e509c6ad4b180a4804f4acf1eaa52b2a64f211a7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 08:29:27 +0400

    Defining base Case

--- src/Array11.java
+++ src/Array11.java
@@ -1,6 +1,9 @@
 public class Array11 {
     public int array11(int[] nums, int index){ //Create a method name
 
+        if (index == nums.length) { //Base Case
+            return 0;
+        }
 
     }
 

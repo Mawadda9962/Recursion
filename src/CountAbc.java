16fa3280d2f6ea0739060beca5e92a25e3bbcaa3
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:36:52 +0400

    Creat another class

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -7,12 +7,11 @@ public class CountAbc {
         String first3 = str.substring(0 ,3); //Checking if the first 3 characters form "abc"
 
         if (first3.equals("abc") || first3.equals("aba")) {
-            return 1 + countAbc(str.substring(1));
+            return 1 + countAbc(str.substring(1)); //Match found, count it and recurse
 
+        }else {
+            return countAbc(str.substring(1)); //No match, just recurse
         }
-
-
-
 
     }
 

9d3937384aaaee54aa1944ee90f6433f74e07b35
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:45:37 +0400

    Defining the base Case

--- src/StrCount.java
+++ src/StrCount.java
@@ -1,6 +1,9 @@
 public class StrCount {
     public int strCount(String str, String sub){ //Creating a method
 
+        if (str.length() < sub.length()){ //Base Case
+            return 0;
+        }
 
     }
 }

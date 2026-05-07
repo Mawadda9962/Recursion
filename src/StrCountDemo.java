3c03dd4cc223b39998e6d00bfdce6ea0fccbf6a8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:59:05 +0400

    Testing the method

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -1,7 +1,7 @@
 public class StrCountDemo {
     public static void main(String[] args){
         StrCount strCount = new StrCount(); //Creating an object
-        System.out.println(strCount.strCount(("catcowcat"), ("cat")));
+        System.out.println(strCount.strCount(("catcowcat"), ("cat"))); //Test the me
 
     }
 }

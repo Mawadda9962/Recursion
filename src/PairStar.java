43d6fdd6c93bba87d3d84d376d76d5edbecd9298
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 08:44:43 +0400

    Defining the Base Case

--- src/PairStar.java
+++ src/PairStar.java
@@ -1,5 +1,7 @@
 public class PairStar {
     public String pairStar(String str){ //Creating a method
-
+        if (str.length() == 0 || str.length() ==1){ //Base Case
+            return str;
+        }
     }
 }

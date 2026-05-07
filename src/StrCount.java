7cf96102406f1303da208d14fcba7e8c1fb811b7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:50:25 +0400

    Conducting the condition  If it does not match

--- src/StrCount.java
+++ src/StrCount.java
@@ -7,7 +7,8 @@ public class StrCount {
        if (str.substring(0,2) == sub){
            return 1 + strCount(str.substring(sub.length()),sub);
        }else {
-//
+           return strCount(str.substring(1),)
+
        }
 
     }

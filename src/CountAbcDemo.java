d12c922b148d6307d2bc43fd1ed2aa7898303605
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:38:45 +0400

    Calling the method

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -2,5 +2,6 @@ public class CountAbcDemo {
 
     public static void main(String[] args){
         CountAbc countAbc = new CountAbc(); //Create an object
+        System.out.println(countAbc.countAbc("abc"));
     }
 }

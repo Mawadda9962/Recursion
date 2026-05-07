c0b8d3853795e4670a47624afde4a0da691603d7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 10:10:01 +0400

    modifiying the main method

--- src/BunnyEarsDemo2.java
+++ src/BunnyEarsDemo2.java
@@ -3,6 +3,5 @@ public class BunnyEarsDemo2 {
         BunnyEars2 bunnyEars2 = new BunnyEars2(); // Creating an object
         System.out.println(bunnyEars2.bunnyEars(0)); //Testing the method
 
-
     }
 }

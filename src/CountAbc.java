480fd9952cd19ceccdb54655074df984e51a8d1a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:41:45 +0400

    Testing the method

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -1,12 +1,13 @@
 public class CountAbc {
     public int countAbc(String str){ //Creating a new method
 
-        if (str.length() < 0){ //Base Case
+        if (str.length() < 3){ //Base Case
             return 0;
         }
-        String first3 = str.substring(0 ,3); //Checking if the first 3 characters form "abc"
+        String first3 = str.substring(0 ,3);
+        String firstC3 = str.substring(0, 3);//Checking if the first 3 characters form "abc"
 
-        if (first3.equals("abc") || first3.equals("aba")) {
+        if (first3.equals("abc") || firstC3.equals("aba")) {
             return 1 + countAbc(str.substring(1)); //Match found, count it and recurse
 
         }else {

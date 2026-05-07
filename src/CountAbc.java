cb4364e2da098419d9a887ae4a5acb4bf99cb87a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:26:04 +0400

    checking if the first 3 characters form aba

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -5,7 +5,7 @@ public class CountAbc {
             return 0;
         }
         String first3 = str.substring(0 ,3); //Checking if the first 3 characters form "abc"
-        String firstC3 = str.substring(0,3);
+        String firstC3 = str.substring(0,3); //
         int count = 0;
 
 

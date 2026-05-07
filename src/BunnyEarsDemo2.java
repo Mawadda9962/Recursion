d6742ec2487c85bb46ae61e4adbf9cdda2c3a71c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 10:09:37 +0400

    Testing the method

--- src/BunnyEarsDemo2.java
+++ src/BunnyEarsDemo2.java
@@ -1,8 +1,7 @@
 public class BunnyEarsDemo2 {
     public static void main(String[] args){
         BunnyEars2 bunnyEars2 = new BunnyEars2(); // Creating an object
-
-        System.out.println(bunnyEars2.bunnyEars(0));
+        System.out.println(bunnyEars2.bunnyEars(0)); //Testing the method
 
 
     }

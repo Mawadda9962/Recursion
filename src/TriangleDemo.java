81683fa8a37f2e7e524b19209ca56adccd2a4d81
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 10:16:28 +0400

    Creating an obj

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -1,6 +1,8 @@
 public class TriangleDemo {
     public static void main(String[] args){
-        Triangle triangle = new Triangle();
+        Triangle triangle = new Triangle(); // Creating an object of the Triangle class
+
+        System.out.println(triangle.triangle());
 
     }
 }

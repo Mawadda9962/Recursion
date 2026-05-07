0eac80a8f32ce1f40c416d51b0b3a8d3fce80df4
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-07 09:24:52 +0400

    Check if the first 3 characters form abc

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -1,9 +1,17 @@
 public class CountAbc {
     public int countAbc(String str){ //Creating a new method
 
-        if (str.length() < 0){
+        if (str.length() < 0){ //Base Case
             return 0;
         }
+        String first3 = str.substring(0 ,3); //Checking if the first 3 characters form "abc"
+        String firstC3 = str.substring(0,3);
+        int count = 0;
+
+
+
+
+
     }
 
 }
